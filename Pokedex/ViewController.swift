ec2d63937516d5f5288b71d03aca018e377c8343
Chen, Yi-Han a <yihan.a.chen@rakuten.com>
2020-04-29 10:01:24 +0900

    Initial Commit

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Chen, Yi-Han a on 2020/04/29.
+//  Copyright © 2020 CS50. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
